9d7a768d1eca0e8958e14a7e83e0dbe1be5925f2
Simon Hyll <hyllsimon@gmail.com>
2024-03-18 19:56:42 +0100

    step 6

--- chatgpt/src-tauri/src/lib.rs
+++ chatgpt/src-tauri/src/lib.rs
@@ -1,5 +1,22 @@
 use log::LevelFilter;
 use tauri_plugin_log::{Target, TargetKind};
+use tauri::{AppHandle, Manager};
+
+#[tauri::command]
+async fn prompt(app: AppHandle, message: String) -> Result<(), ()> {
+  log::info!("Received message!");
+  tauri::async_runtime::spawn(async move {
+    let window = app.clone().get_webview_window("main").unwrap();
+    let _ = window.emit("message", "Hello!");
+  });
+  Ok(())
+}
+
+#[tauri::command]
+async fn connect(app: AppHandle, token: String) -> Result<(), ()> {
+  log::info!("Connecting!");
+  Ok(())
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -13,6 +30,7 @@ pub fn run() {
                 ])
                 .build(),
         )
+        .invoke_handler(tauri::generate_handler![prompt, connect])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
