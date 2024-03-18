4745c5fa80b016a3246740c4c718c08aa033a63a
Simon Hyll <hyllsimon@gmail.com>
2024-03-18 19:09:52 +0100

    step 5

--- chatgpt/app.vue
+++ chatgpt/app.vue
@@ -1,3 +1,8 @@
 <template>
   <NuxtLayout />
 </template>
+
+<script>
+import { attachConsole } from "@tauri-apps/plugin-log";
+const detach = await attachConsole();
+</script>
--- chatgpt/src-tauri/src/lib.rs
+++ chatgpt/src-tauri/src/lib.rs
@@ -1,6 +1,18 @@
+use log::LevelFilter;
+use tauri_plugin_log::{Target, TargetKind};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .level(LevelFilter::Info)
+                .targets([
+                    Target::new(TargetKind::Stdout),
+                    Target::new(TargetKind::Webview),
+                ])
+                .build(),
+        )
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
